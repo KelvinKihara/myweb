f0d825a69083a1de8e11b2552f0a498f488ef3aa
kelvin kihara <kiharkelvinm@gmail.com>
2025-11-26 18:14:01 +0300

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+let myName = "Kelvin";
+let myAge = 30;
+let myHobby = "coding";
+
+// template string
+console.log(`My name is ${myName} and I am ${myAge} years old. My hobby is ${myHobby}.`);
+
+console.log(after the script tag);
+
