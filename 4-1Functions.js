5b3e461f9b0bc8051456bcfad705d0c2855f4b63
kelvin kihara <kiharkelvinm@gmail.com>
2025-12-16 11:08:10 +0300

    exe practise

--- 4-1Functions.js
+++ 4-1Functions.js
@@ -1,6 +1,42 @@
-// function with multiple parameters
-function add(name, age, hobby) {
-  console.log(`My name is" + (name + age + hobby)  );
+
+// JavaScript reads the code top to bottom.
+// JavaScript does not execute the function now
+
+// It just stores the function in memory for later use
+// Executed immediately
+// console.log("Hey there");
+
+
+console.log("Hey there");
+
+
+function callMe() {
+    console.log("i have been called");
 }
 
-introduce("Kelvin", 30, "coding");
+callMe();
+
+
+console.log("Hey");
+
+
+// Arrow Functions Function Expression not hoisted 
+// const greet = (name) => {
+//     console.log(`Hello, ${name}`);
+// }
+
+// greet("Kelvin");
+
+// let and const are not accessible before declaration (they exist in the temporal dead zone).
+
+// sayHello()
+// const sayHello = () {
+//     console.log("Hello kk");
+// }
+
+// sayHello();
+
+// let sayHello = function () {
+//   console.log("Hello");
+// };
+
